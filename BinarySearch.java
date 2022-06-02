be302971d3e382b04f98b55803c22ced95964cb3
J-the-new-man <myself.jamirulislam@gmail.com>
2022-06-02 22:16:45 +0530

    java file was add

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch{
+    public static void name() {
+        int[] nums = {0,1,2,3,45,6,78,9,} ;
+        int target = 5 ;
+
+        System.out.println(binarySearch(nums , target));
+    }
+    static int binarySearch(int[] arr ,int target){
+        int start = 0 ;
+        int end = arr.length -1 ;
+
+        while(start<=end){
+            int mid = start + (end-start)/2 ;
+            if(target>arr[mid]){
+                start = mid + 1 ;
+            
+            }
+            if(target<arr[mid]){
+                end = mid - 1 ;
+             
+            }
+            if(target==arr[mid]){
+                return mid ;
+            }
+        }
+
+
+        return -1 ;
+
+    }
+}
